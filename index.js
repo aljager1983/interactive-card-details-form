2e8a0d5185cf82c558d4ff81d43c63c357e24caa
promdiGamer <al_jager1983@yahoo.com>
2023-08-11 21:39:59 +0800

    8.updating js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const cname = document.getElementById("name");
 const cardNumPreview = document.getElementById("cardNumPreview");
 const expDate = document.getElementById("expDate");
 const cvv = document.getElementById("cvv");
+const monthPrev = document.getElementById("monthPrev");
+const yearPrev = document.getElementById("yearPrev");
 
 const form = document.getElementById("details-form");
 const success = document.getElementById("success");
@@ -30,17 +32,44 @@ function size () {
 confButton.addEventListener("click", function() {
     form.style.display = "none";
     success.style.display = "flex";
-    cname.innerHTML = cardName.value;
-    cardNumPreview.innerHTML = cardNumber.value;
+    // cname.innerHTML = cardName.value;
+    // cardNumPreview.innerHTML = cardNumber.value;
     expDate.innerHTML = month.value + "/" + year.value;
     cvv.innerHTML = cvc.value;
 })
 
+// cardNumPreview.innerHTML = arr.join("");
 //continue /  resets page
 contButton.addEventListener("click", function() {
     location.reload();
     success.style.display = "none"
 })
 
+function updateName() {
+    cname.innerHTML = cardName.value;
+}
 
+cardNumber.addEventListener("keyup", function() {
+    const cardNumPrevMatch = cardNumber.value;
+    const updatePrevNum = cardNumPrevMatch.match(/.{1,4}/g);
+    cardNumPreview.innerHTML = updatePrevNum.join(" ");
 
+    //trim / limit the inputs
+    if(cardNumber.value.length > 15) {
+        cardNumber.value = cardNumber.value.slice(0, 15)
+    }
+   console.log(cardNumber.value.length);
+})
+
+month.addEventListener("keyup", function() {
+    monthPrev.innerHTML = month.value;
+    
+})
+
+year.addEventListener("keyup", function() {
+    yearPrev.innerHTML = year.value;
+})
+
+cvc.addEventListener("keyup", function() {
+    cvv.innerHTML = cvc.value;
+})
